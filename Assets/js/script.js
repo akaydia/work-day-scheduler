b6b9a9a3ac65f5deced555166b12768c50a999bc
Akaydia <akaydiaaa@hotmail.com>
2023-01-26 19:00:04 -0500

    added a getOrdinalSuffix function, able to present the current day

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,31 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+// variables
+let currentDayEl = $("#currentDay");
+
+// function to add suffix according to the value of the currentDay
+function getOrdinalSuffix(i) {
+  let j = i % 10;
+  let k = i % 100;
+  if (j == 1 && k != 11) {
+      return i + "st";
+  }
+  if (j == 2 && k != 12) {
+      return i + "nd";
+  }
+  if (j == 3 && k != 13) {
+      return i + "rd";
+  }
+  return i + "th";
+}; // getOrdinalSuffix()
+
+// presenting the currentDay
+let currentDay = `${dayjs().format('dddd, MMMM ')}${getOrdinalSuffix(dayjs().date())}`;
+currentDayEl.text(currentDay);
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
